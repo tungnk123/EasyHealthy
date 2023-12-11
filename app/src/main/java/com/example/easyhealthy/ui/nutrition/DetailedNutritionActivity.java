2a8bb424ba4c40c8afc5567772f4f9d91a6c7e90
tungnk123 <doanthanhtungnk123@gmail.com>
2023-12-11 22:41:40 +0700

    Add bar chart dependencies and test bar chart

--- app/src/main/java/com/example/easyhealthy/ui/nutrition/DetailedNutritionActivity.java
+++ app/src/main/java/com/example/easyhealthy/ui/nutrition/DetailedNutritionActivity.java
@@ -8,10 +8,17 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.easyhealthy.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 public class DetailedNutritionActivity extends AppCompatActivity {
 
     Button btnThemDuLieu;
+    BarChart barChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +30,21 @@ public class DetailedNutritionActivity extends AppCompatActivity {
 
 
     private void addControls() {
+
         btnThemDuLieu = (Button) findViewById(R.id.btn_themDuLieu);
+        barChart = findViewById(R.id.chart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 45f));
+        entries.add(new BarEntry(0, 65f));
+        entries.add(new BarEntry(0, 75f));
+        entries.add(new BarEntry(0, 85f));
+        entries.add(new BarEntry(0, 105f));
+
+        BarDataSet dataSet = new BarDataSet(entries, "Subjects");
+        dataSet.setColor(R.color.purple_500);
+        BarData barData = new BarData(dataSet);
+        barChart.setData(barData);
     }
 
     private void addEvents() {
